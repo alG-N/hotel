f0cb82f243fba67bba96eb702860795e42d8f9b4
alG-N <whittylord@gmail.com>
2026-02-02 14:04:51 +0700

    fix: add missing revalidateTheme hook

---
+++ src/globals/ThemeSettings/hooks/revalidateTheme.ts
@@ -0,0 +1,10 @@
+import type { GlobalAfterChangeHook } from 'payload'
+import { revalidateTag } from 'next/cache'
+
+export const revalidateTheme: GlobalAfterChangeHook = ({ doc, req: { payload } }) => {
+  payload.logger.info(`Revalidating theme settings`)
+
+  revalidateTag('global_theme-settings')
+
+  return doc
+}
