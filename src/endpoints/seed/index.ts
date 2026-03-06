4df3b430385e96a6ab2754ee12965bc102231118
alG-N <whittylord@gmail.com>
2026-03-07 05:23:27 +0700

    fix: correct typo in comment regarding revalidation error messages

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -5,7 +5,7 @@ import { seedHotelData } from './hotel-data'
 // Next.js revalidation errors are normal when seeding the database without a server running
 // i.e. running `yarn seed` locally instead of using the admin UI within an active app
 // The app is not running to revalidate the pages and so the API routes are not available
-// These error messages can be ignored: `Error hitting revalidate route for...`
+// These error messages can be ignored: `Error hitting revalidate route for....`
 export const seed = async ({
   payload,
   req,
